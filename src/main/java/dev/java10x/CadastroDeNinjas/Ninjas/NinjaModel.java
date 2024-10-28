494d321b518daf0ab9518686cd8d2695950ce46e
Idelcio <idelcioforest@gmail.com>
2024-10-28 14:04:41 -0300

    Modelagem do banco de dados, NinjaModel e MissoesModel Concluidos, anotations feitas para relacionamentos de tabelas

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,7 +1,10 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
@@ -9,10 +12,20 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long id;
-    String nome;
-    String email;
-    int idade;
+    private long id;
+
+    private String nome;
+
+    private String email;
+
+    private int idade;
+
+    // @ManyToOne um ninja tem uma única missão
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") // Foreing Key - chave estrangeira
+    private MissoesModel missoes;
+
+
 
     public NinjaModel() {
     }
